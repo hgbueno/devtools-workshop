2ce984feabc28a76101ee0105b87a3afe5e88ab2
Henrique Bueno <buenoh@amazon.com>
2020-09-07 15:30:06 -0300

    adjusts

--- sample-app/app/main.py
+++ sample-app/app/main.py
@@ -4,19 +4,22 @@ from flask import Flask
 app = Flask(__name__)
 
 mypath="myapp"
-@app.route("/{mypath}")
+
+@app.route("/"+mypath)
 def hello():
     html = "<h3>Sample App | Launcher </h3>" \
-            "<h2>Microservice: CHANGE_PATH</h2>" \
+            "<h2>Microservice: {full_path}</h2>" \
            "<b>Hostname:</b>{hostname}<br/>" \
            "<b>IP Address:</b>{ip_address}<br/>"
-    return html.format(hostname=socket.gethostname(),ip_address=socket.gethostbyname(socket.gethostname()))
+    return html.format(full_path=get_path(mypath),hostname=socket.gethostname(),ip_address=socket.gethostbyname(socket.gethostname()))
 
-class TestGetHostName(unittest.TestCase):
-            def test_get_hostname(self):
-                self.assertEqual("myapp" , mypath, "nao eh")
+def get_path(path):
+    new_path="/"+path
+    return new_path
 
+class TestGetPath(unittest.TestCase):
+            def test_get_path(self):
+                self.assertEqual("/test", get_path("test"), "Teste Unitário falhou")
 
 if __name__ == "__main__":
-    unittest.main()
     app.run(host='0.0.0.0')
